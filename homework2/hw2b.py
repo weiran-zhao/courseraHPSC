3cd14151fc27860c4851045e69fd618d177b2712
Ryan Zhao <zhaoweiranregister@gmail.com>
2013-05-17 22:05:01 -0400

    finish homework2

--- homework2/hw2b.py
+++ homework2/hw2b.py
@@ -143,8 +143,16 @@ def plot_cubic(xi,yi):
     and saves the resulting figure as cubic.png
     """
 
+    # check inputs and print error message if not valid:
+
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have length 4"
+    assert len(xi)==4 and len(yi)==4, error_message
+
     x = np.linspace(xi.min()-1,xi.max()+1,1001) # points to evaluate polynomial
-    c = quad_interp(xi,yi)  #calculate c
+    c = cubic_interp(xi,yi)  #calculate c
     y = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
     
     plt.figure(1)       # open plot figure window
@@ -155,7 +163,7 @@ def plot_cubic(xi,yi):
     plt.plot(xi,yi,'ro')   # plot as red circles
     plt.ylim(yi.min()-1,yi.max()+1)         # set limits in y for plot
     
-    plt.title("Data points and interpolating polynomial for quardratic " \
+    plt.title("Data points and interpolating polynomial for cubic" \
               "polynomials")
     
     plt.savefig('cubic.png')   # save figure as .png file
@@ -172,5 +180,99 @@ def test_cubic1():
         A = np.array([np.ones(4),xi,xi*xi,xi*xi*xi]).T
         yi = np.dot(A,c_true)
         c = cubic_interp(xi,yi)
+        """
+        *********************************************
+        Be careful with the numerical stability as 
+        xi***3 may blow off and give an assersion error
+        *********************************************
+        """
         assert np.allclose(c,c_true), \
                 "Incorrect result, c  = %s, Expected: c = %s" % (c,c_true)
+
+"""
+Dealing with polynomial interpolation
+"""
+def poly_interp(xi,yi):
+    """
+    Given xi and yi of length n, do a n-1 degree polynomial interpolation 
+    """
+    # Check inputs 
+    
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have the same length"
+    assert len(xi) == len(yi), error_message
+
+    # Set up matrix A using Horner's method
+    size = len(xi)
+    A = np.ones((size,size))
+    for i in range(size):
+        A[:,i]*=xi**i
+
+    # Set up vector b
+    b = yi
+
+    # Solve the equation
+    c = solve(A,b)
+
+    return c
+
+def plot_poly(xi,yi):
+    """
+    takes two numpy arrays xi and yi of the same length, calls poly_interp to compute 
+    c, and then plots both the interpolating polynomial and the data points, 
+    and saves the resulting figure as poly.png
+    """
+
+    # check inputs and print error message if not valid:
+
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have the same length"
+    assert len(xi) == len(yi), error_message
+
+    x = np.linspace(xi.min()-1,xi.max()+1,1001) # points to evaluate polynomial
+    c = poly_interp(xi,yi)  #calculate c
+    size = len(xi)
+    y= c[size-1]
+    for i in range(size-1,0,-1):
+        y = y*x + c[i-1]
+
+    plt.figure(1)       # open plot figure window
+    plt.clf()           # clear figure
+    plt.plot(x,y,'b-')  # connect points with a blue line
+    
+    # Add data points  (polynomial should go through these points!)
+    plt.plot(xi,yi,'ro')   # plot as red circles
+    plt.ylim(yi.min()-1,yi.max()+1)         # set limits in y for plot
+    
+    plt.title("Data points and interpolating polynomial for n-degree" \
+              "polynomials")
+    
+    plt.savefig('poly.png')   # save figure as .png file
+
+def poly_test():
+    """
+    Do a number of tests for poly_interp
+    1. generate a random number between 4 and 6 for length of array
+    2. test 10 times for the length with randomly generated xi and yi
+    3. do this for 10 times
+    """
+    testSize = 10
+    scale = 10
+    for i in range(testSize):
+        arraySize = np.random.randint(4, 6, 1)
+        print "test size ", arraySize
+        for z in range(10):
+            xi = np.random.rand(arraySize)*scale-scale
+            c_true = np.random.rand(arraySize)-0.5
+            # Set up matrix A using Horner's method
+            A = np.ones((arraySize,arraySize))
+            for j in range(arraySize):
+                A[:,j]*=xi**j
+            yi = np.dot(A, c_true)
+            c = poly_interp(xi,yi)
+            assert np.allclose(c,c_true), \
+            "Incorrect result, c  = %s, Expected: c = %s" % (c,c_true)
