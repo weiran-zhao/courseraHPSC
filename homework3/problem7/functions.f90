06c9b242ebbc8669d2924a4f6fe8db35462c0d2b
Ryan Zhao <zhaoweiranregister@gmail.com>
2013-06-03 20:32:01 -0400

    start question 7 of hw3

---
+++ homework3/problem7/functions.f90
@@ -0,0 +1,12 @@
+! Originally in $UWHPSC/codes/fortran/newton/functions.f90
+! Modified by Ryan (Weiran) Zhao to question 7 of homework 3
+! Mon,Jun 03th 2013 08:30:02 PM EDT
+
+module functions
+
+contains
+
+real(kind=8) function 
+
+
+end module functions
