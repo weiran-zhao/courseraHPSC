8ebe84c38e6e5eb655ced90393231801febb3d35
Ryan Zhao <zhaoweiranregister@gmail.com>
2013-06-26 21:44:34 -0400

    finish part 4 of homework4

---
+++ homework4/quadrature_omp.f90
@@ -0,0 +1,68 @@
+! Modified from quadrature.f90
+! Ryan (Weiran) Zhao 
+! Wed,Jun 26th 2013 09:19:37 PM EDT
+module quadrature_omp
+    use omp_lib
+    implicit none
+
+contains
+
+    real(kind=8) function trapezoid(f,a,b,n)
+        ! used to calculate numerical quaduture of function 'f'
+        ! using trapezoidal method
+        ! Arguments:
+        ! f:    function to be integrated
+        ! a:    starting point of function to be evaluated from
+        ! b:    end point
+        ! n:    number of points to use, the bigger n, the more accurate
+
+        implicit none
+        real(kind=8), intent(in)::  a, b
+        real(kind=8), external:: f
+        integer, intent(in):: n
+        ! local variables
+        real(kind=8) h
+        real(kind=8), dimension(n) :: x_val, f_val
+        integer :: i
+
+        h = (b-a)/(n-1)
+        do i=1,n
+            x_val(i) = a+(i-1)*h
+            f_val(i) = f(x_val(i))
+        end do
+
+        ! sum up
+        trapezoid = -0.5*h*(f_val(1)+f_val(n))
+        !$omp parallel do reduction(+: trapezoid)
+        do i=1,n
+            trapezoid=trapezoid+h*f_val(i)
+        end do
+    end function trapezoid
+
+    subroutine error_table(f,a,b,nvals,int_true)
+        implicit none
+        ! print error tables for trapezoidal method 
+        ! for different number of evaluation points
+        ! Arguments:
+        !   f, a, b:    same meanings as in function 'trapezoid'
+        !   nvals:      different number of evaluation points to try
+        !   int_true:   true value of the integration, normally you don't have
+
+        real(kind=8), intent(in):: a,b,int_true
+        real(kind=8), external:: f
+        integer, dimension(:), intent(in) :: nvals
+        ! Local variables
+        real(kind=8) :: last_error=0.d0, int_trap, error, ratio
+        integer :: i
+
+        print *, "    n         trapezoid            error       ratio"
+        do i=1,size(nvals)
+            int_trap = trapezoid(f,a,b,nvals(i))
+            error = dabs(int_trap-int_true)
+            ratio = last_error/error
+            last_error = error
+            print 11, nvals(i), int_trap, error, ratio
+            11     format(i8, es22.14, es13.3, es13.3)
+        end do
+    end subroutine error_table
+end module quadrature_omp
---
+++ homework4/test2_omp.f90
@@ -0,0 +1,40 @@
+! Modified from test2.f90
+! Ryan (Weiran) Zhao 
+! Wed,Jun 26th 2013 09:19:49 PM EDT
+program test2
+
+    use quadrature_omp, only: trapezoid, error_table
+    use omp_lib
+    implicit none
+    real(kind=8) :: a,b,int_true
+    integer :: nvals(12), i
+    ! add ocsilation impact k
+    integer :: k=1000
+    !$ print *, "using omp with 2 threads"
+    !$ call omp_set_num_threads(2)
+
+    a = 0.d0
+    b = 2.d0
+    int_true = (b-a) + (b**4 - a**4) / 4.d0 - (1.d0/k)* (dcos(k*b)-dcos(k*a))
+
+    print 10, int_true
+    10 format("true integral: ", es22.14)
+    print *, " "  ! blank line
+
+    ! values of n to test:
+    do i=1,12
+    nvals(i) = 5 * 2**(i-1)
+    enddo
+
+    call error_table(f, a, b, nvals, int_true)
+
+contains
+
+    real(kind=8) function f(x)
+        implicit none
+        real(kind=8), intent(in) :: x 
+
+        f = 1.d0 + x**3 + dsin(1000*x)
+    end function f
+
+end program test2
